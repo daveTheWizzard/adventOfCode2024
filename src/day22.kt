10ccf7377e1343412dc290928209de33928d90a9
David Meichel <davidmeichel@gmx.de>
2024-12-22 10:40:15 +0100

    Implement part 2 of day 22

--- src/day22.kt
+++ src/day22.kt
@@ -1,16 +1,50 @@
+import helpers.*
+
 fun main(){
-    val buyerSecrets = helpers.readInput("input_day22").map{ it.toLong()}
-    var result: Long = 0
-    buyerSecrets.forEach {
-        var secret = it
-        repeat(2000) {
-            secret = getNextSecretNumber(secret)
+    val buyerSecrets = readInput("input_day22").map{ it.toLong()}
+    val prefixesLeadingToBananas = buildMap {
+        buyerSecrets.forEachIndexed { secretID, secret ->
+            val prefixesToBananas = getPrefixesForSecret(secret)
+            prefixesToBananas.forEach { prefix, numberBananas ->
+                val prefixWithID = prefix.toMutableList()
+                prefixWithID.add(0, secretID)
+                put(prefixWithID, numberBananas)
+            }
         }
-        println("Result for secret $it is $secret")
-        result += secret
     }
+    val prefixedLeadingToBananasAsList = prefixesLeadingToBananas.map { (prefix, bananas) ->
+        var p = prefix.toMutableList()
+        p.add(bananas)
+        p
+    }
+    val groupedPrefixes = prefixedLeadingToBananasAsList.groupBy { it.subList(1,5) }
+    val prefixesToNumberOfBananas = groupedPrefixes.map { (sublist, fullList) ->
+       fullList.sumOf { it.last() }
+    }
+    val result = prefixesToNumberOfBananas.max()
     println("Result: $result")
+    //prefixesToBananas.forEach{println(it)}
 }
+
+private fun getPrefixesForSecret(secretNumber: Long): MutableMap<List<Int>, Int> {
+    val sequence = getSecretNumberSequence(secretNumber)
+    val result: MutableMap<List<Int>, Int> = mutableMapOf()
+    for(i in 4..sequence.lastIndex){
+        val changesLeadingToSecret = getChangesLeadingToSecret(sequence, i)
+        if(!result.contains(changesLeadingToSecret)) { // Monkeys use the first occurrence of the secret
+            result[changesLeadingToSecret] = sequence[i].mod(10)
+        }
+    }
+    return result
+}
+
+private fun getChangesLeadingToSecret(sequence: List<Long>, secretIndex: Int): List<Int>{
+    require(secretIndex >= 4)
+    val lastDigits = sequence.subList(secretIndex-4, secretIndex+1).map{it.mod(10)}
+    val changes = lastDigits.zipWithNext().map { it.second - it.first }
+    return changes
+}
+
 private fun getSecretNumberSequence(secretNumber: Long): List<Long>{
    return buildList {
        var secret = secretNumber
